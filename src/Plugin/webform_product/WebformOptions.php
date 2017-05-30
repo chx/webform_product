4710aab46dba74dcc9796e715601161683b34129
Karoly Negyesi <karoly@negyesi.net>
2017-05-29 21:32:07 -0700

    Further simplification, cart written but untested

--- src/Plugin/webform_product/WebformOptions.php
+++ src/Plugin/webform_product/WebformOptions.php
@@ -1,22 +1,16 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\webform_product\Plugin\webform_product\WebformOptions.
- */
-
-
 namespace Drupal\webform_product\Plugin\webform_product;
 
-use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\webform_product\WebFormProductFormHelper;
 
 /**
  * @PluginID("webform_options")
  */
 class WebformOptions {
 
-  public static function process(&$element, FormStateInterface $form_state, &$complete_form) {
+  public static function process(&$element, FormStateInterface $form_state) {
     $element['options']['#element']['price'] = [
       '#type' => 'textfield',
       '#title' => t('Price'),
@@ -24,13 +18,11 @@ class WebformOptions {
       '#placeholder' => '99.99',
       '#maxlength' => 20,
     ];
-
-    /** @var \Drupal\webform\Entity\Webform $webform */
-    $webform = $form_state->getFormObject()->getWebForm();
-    $prices = (array) $webform->getThirdPartySetting('webform_product', $webform->id() . '/' . $element['#name']);
-    foreach ($element['options']['#default_value'] as $delta => $row) {
-      if (isset($prices[$row['value']])) {
-        $element['options']['#default_value'][$delta]['price'] = $prices[$row['value']];
+    if ($prices = WebFormProductFormHelper::getSetting($form_state, 'options')) {
+      foreach ($element['options']['#default_value'] as $delta => $row) {
+        if (isset($prices[$row['value']])) {
+          $element['options']['#default_value'][$delta]['price'] = $prices[$row['value']];
+        }
       }
     }
     // WebFormOptions::convertValuesToOptions() destroys our values so do
@@ -39,7 +31,7 @@ class WebformOptions {
     return $element;
   }
 
-  public static function convertToSettings($element, FormStateInterface $form_state, $complete_form) {
+  public static function convertToSettings($element, FormStateInterface $form_state) {
     foreach ($element['options']['#value'] as $value) {
       if (!empty($value['price'])) {
         $option_value = $value['value'];
@@ -49,12 +41,10 @@ class WebformOptions {
         if ($option_value === '') {
           $option_value = $option_text;
         }
-        $settings[$option_value] = $value['price'];
+        $prices[$option_value] = $value['price'];
       }
-      if (!empty($settings)) {
-        /** @var \Drupal\webform\Entity\Webform $webform */
-        $webform = $form_state->getFormObject()->getWebForm();
-        $webform->setThirdPartySetting('webform_product', $webform->id() . '/' . $element['#name'], $settings);
+      if (!empty($prices)) {
+        WebFormProductFormHelper::setSetting($form_state, 'options', $prices);
       }
     }
   }
--- src/WebFormProductFormHelper.php
+++ src/WebFormProductFormHelper.php
@@ -2,93 +2,80 @@
 
 namespace Drupal\webform_product;
 
-use Drupal\commerce_cart\Exception\DuplicateCartException;
-use Drupal\commerce_product\Entity\ProductVariation;
+use Drupal\commerce_order\Entity\OrderItem;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Url;
 
 class WebFormProductFormHelper {
 
-  static public function processElementForm(&$element, FormStateInterface $form_state, &$complete_form) {
-    if (empty($element['#value'])) {
-      return $element;
-    }
-    /** @var \Drupal\webform_ui\Form\WebformUiElementFormInterface  $formObject */
-    $formObject = $form_state->getFormObject();
-    $skuRoot = $formObject->getWebform()->id() . ':' . $formObject->getKey();
-    $entityStorage = \Drupal::entityTypeManager()->getStorage('commerce_product_variation');
-    $query = $entityStorage->getQuery()->condition('type', 'webform');
-    $skuCondition = $query
-      ->orConditionGroup()
-      ->condition('sku', $skuRoot)
-      ->condition('sku', "$skuRoot:", 'STARTS_WITH');
-    $ids = $query->condition($skuCondition)->execute();
-    /** @var \Drupal\commerce_product\Entity\ProductVariation  $variation */
-    foreach ($entityStorage->loadMultiple($ids) as $variation) {
-      $complete_form['#webform_product_links'][$variation->getSku()] = [
-        '#type' => 'link',
-        '#title' => t('Price: @price', ['@price' => $variation->getPrice()]),
-        '#url' => $variation->toUrl('edit-form'),
-      ];
-    }
-    if (isset($complete_form['#webform_product_links'][$skuRoot])) {
-      $complete_form['properties']['element']['webform_product'] = $complete_form['#webform_product_links'][$skuRoot];
-    }
-    else {
-      $complete_form['properties']['element']['webform_product'] = [
-        '#type' => 'link',
-        '#url' => static::getUrl($skuRoot, $element['#value']),
-        '#title' => t('Create product'),
-      ];
-    }
+  public static function processElementForm(&$element, FormStateInterface $form_state, &$complete_form) {
+    $element['price'] = [
+      '#type' => 'textfield',
+      '#title' => t('Price'),
+      '#placeholder' => '99.99',
+      '#maxlength' => 20,
+      '#default_value' => static::getSetting($form_state, 'top'),
+      '#element_validate' => [[get_class(), 'saveTopPrice']],
+    ];
     return $element;
   }
 
-  public static function submissionToCart(array &$form, FormStateInterface $form_state) {
-    $sid = $form_state->getFormObject()->getEntity()->id();
-    $skus = [];
-    foreach (db_query('SELECT * FROM {webform_submission_data} WHERE sid = :sid', [':sid' => $sid]) as $row) {
-      // The same SKU might be repeated.
-      $skus[] = "$row->webform_id:$row->name";
-      $skus[] = "$row->webform_id:$row->name:$row->value";
-    }
-    $variations = [];
-    $variationIds = \Drupal::entityQuery('commerce_product_variation')
-      ->condition('type', 'webform')
-      ->condition('sku', $skus, 'IN')
-      ->execute();
-    foreach (ProductVariation::loadMultiple($variationIds) as $variation) {
-    /** @var \Drupal\commerce_product\Entity\ProductVariation $variation */
-      $variations[$variation->getSku()] = $variation;
-    }
-    if ($skus = array_intersect($skus, array_keys($variations))) {
-      $store = \Drupal::service('commerce_store.store_context')->getStore();
-      $cartProvider = \Drupal::service('commerce_cart.cart_provider');
-      $cartOrder = $cartProvider->getCart('default', $store) ?: $cartProvider->createCart('default', $store);
-      /** @var \Drupal\commerce_cart\CartManager $cartManager */
-      $cartManager = \Drupal::service('commerce_cart.cart_manager');
-      foreach ($skus as $sku) {
-        $cartManager->addEntity($cartOrder, $variations[$sku]);
-      }
-      $form_state->setRedirect('commerce_cart.page');
-    }
+  public static function saveTopPrice($element, FormStateInterface $form_state) {
+    static::setSetting($form_state, 'top', $element['#value']);
   }
 
-  /**
-   * @param $sku
-   * @param $title
-   *
-   * @return \Drupal\Core\Url
-   */
-  public static function getUrl($sku, $title) {
-    $url = Url::fromRoute('webform_product.page', [
-      // urlencoding is completely rotten: a single urlencoding will break the
-      // path regexp in Router::doMatchCollection() when one of the arguments
-      // contain a slash and double urlencoding breaks the CSRF checker...
-      'sku' => base64_encode($sku),
-      'title' => base64_encode($title),
-    ]);
-    return $url;
+  public static function getSetting(FormStateInterface $form_state, $settingKey) {
+    /** @var \Drupal\webform_ui\Form\WebformUiElementEditForm $formObject */
+    $formObject = $form_state->getFormObject();
+    $setting = $formObject->getWebform()->getThirdPartySetting('webform_product', $formObject->getKey());
+    return isset($setting[$settingKey]) ? $setting[$settingKey] : NULL;
+  }
+
+  public static function setSetting(FormStateInterface $form_state, $settingKey, $value) {
+    /** @var \Drupal\webform_ui\Form\WebformUiElementEditForm $formObject */
+    $formObject = $form_state->getFormObject();
+    $webform = $formObject->getWebform();
+    $elementKey = $formObject->getKey();
+    $setting = $webform->getThirdPartySetting('webform_product', $elementKey);
+    $setting[$settingKey] = $value;
+    $webform->setThirdPartySetting('webform_product', $elementKey, $setting);
+  }
+
+  public static function submissionToCart(array &$form, FormStateInterface $form_state) {
+    /** @var \Drupal\webform\WebformSubmissionInterface $submission */
+    $submission = $form_state->getFormObject()->getEntity();
+    $webform = $submission->getWebform();
+    if (!$prices = $webform->getThirdPartySettings('webform_product')) {
+      return;
+    }
+    $store = \Drupal::service('commerce_store.store_context')->getStore();
+    $cartProvider = \Drupal::service('commerce_cart.cart_provider');
+    /** @var \Drupal\commerce_order\Entity\OrderInterface $cartOrder */
+    $cartOrder = $cartProvider->getCart('default', $store) ?: $cartProvider->createCart('default', $store);
+    $elements = $webform->getElementsInitializedAndFlattened();
+    $prices = $webform->getThirdPartySettings('webform_product');
+    foreach ($submission->getData() as $key => $value) {
+      if (isset($prices[$key])) {
+        if (!empty($prices[$key]['top'])) {
+          $orderItem = OrderItem::create([
+            'title' => $elements[$key]['#title'],
+            'unit_price' => ['number' => $prices[$key]['top']]
+          ]);
+          $orderItem->save();
+          $cartOrder->addItem($orderItem);
+        }
+        if (!empty($prices[$key]['options'])) {
+          foreach (array_keys($value) as $option) {
+            $orderItem = OrderItem::create([
+              'title' => $elements[$key]['#options'][$option],
+              'unit_price' => ['number' => $prices[$key]['options'][$option]]
+            ]);
+            $orderItem->save();
+            $cartOrder->addItem($orderItem);
+          }
+        }
+      }
+    }
+    $form_state->setRedirect('commerce_cart.page');
   }
 
 }
